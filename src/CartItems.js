e589e4d338c3404e5365ded9984930989e4d7403
Shiv <iamshivanand@users.noreply.github.com>
2020-07-21 22:55:05 +0530

    added the quantity increase function only

--- src/CartItems.js
+++ src/CartItems.js
@@ -11,6 +11,23 @@ class cartItem extends React.Component{
             Qty:1,
             img:''
         }
+        // this.increaseQuantity= this.increaseQuantity.bind(this)
+        //this is the another way of binding the this so that this should not be undefined
+        //but this might also get pretty messy so instead we use arrow function as arrow function themself bind  this 
+
+    }
+    //below we use the arrow function for the binding purpose 
+    // so whereever we find any problem regarding the binding then we can use the arrow function to rsolve any such problem
+    
+    increaseQuantity = () => {
+        // console.log("this.state",this.state);
+        console.log("new Quantity", ++this.state.Qty);
+    }
+    decreaseQuantity(){
+        console.log("decrease");
+    }
+    deleteProduct(){
+        console.log("Delete");
     }
     render(){
         const {price, tittle, Qty }= this.state;
@@ -26,9 +43,30 @@ class cartItem extends React.Component{
                     <div>Qty : {Qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/864/864378.svg" />
-                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/972/972575.svg" />
-                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/3096/3096687.svg" />
+                        <img
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/864/864378.svg" 
+                            onClick={this.increaseQuantity}
+                            // onClick={this.increaseQuantity.bind(this)}
+                            //we use bind because otherwise when we are calling increaseQuantity the value of this is lost 
+                            //so we need to bind the value of this wit the function otherwise it will through an error as this is not defined
+                            //instead of binding over over here we can aslo bind it in constructor when there are multiple event handlers
+
+
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/svg/972/972575.svg" 
+                            onClick= {this.decreaseQuantity}
+                        />
+                        <img 
+                            alt="delete"
+                            className="action-icons"
+                            src="https://image.flaticon.com/icons/svg/3096/3096687.svg" 
+                            onClick= {this.deleteProduct}
+                        />
 
                     </div>
                 </div>
