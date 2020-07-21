02625b54d6adabce6296c95c03dcabfaf962d001
Shiv <iamshivanand@users.noreply.github.com>
2020-07-21 22:26:04 +0530

    added the state

--- src/CartItems.js
+++ src/CartItems.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 class cartItem extends React.Component{
+    //adding the state
+    constructor () {
+        //we need to also call superclass as we extends from super class react.components
+        super();
+        this.state = {
+            price: 999,
+            tittle: 'Phone',
+            Qty:1,
+            img:''
+        }
+    }
     render(){
+        const {price, tittle, Qty }= this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -9,11 +21,15 @@ class cartItem extends React.Component{
                   
                 </div>
                 <div className="right-block">
-                    <div style={{fontFamily:"cursive", fontSize:20}}>Phone</div>
-                    <div>Rs 9999</div>
-                    <div>Qty: 1</div>
+                    <div style={{fontFamily:"cursive", fontSize:20}}>{tittle}</div>
+                    <div>RS {price}</div>
+                    <div>Qty : {Qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/864/864378.svg" />
+                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/972/972575.svg" />
+                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/3096/3096687.svg" />
+
                     </div>
                 </div>
             </div>
@@ -24,7 +40,8 @@ const styles= {
     image: {
         height:110,
         width:100,
-        borderRadius:4
+        borderRadius:4,
+        backgroundColor:'grey'
     }
 }
 export default cartItem;
