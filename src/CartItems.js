97b11d6074e0a92eacd87b143b165db27a682ec7
Shiv <iamshivanand@users.noreply.github.com>
2020-07-22 22:55:52 +0530

    added the state and its working to increase and decrease the the quantity

--- src/CartItems.js
+++ src/CartItems.js
@@ -15,16 +15,79 @@ class cartItem extends React.Component{
         //this is the another way of binding the this so that this should not be undefined
         //but this might also get pretty messy so instead we use arrow function as arrow function themself bind  this 
 
+
+            //in some cases like when we are making ajax call or if we are using promises so inside these react wont do 
+            //batching for us  state call will act like syncronus call 
+            // this.testing();
     }
+    // testing(){
+    //     const promise= new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve('done');
+    //         },5000);
+    //     })
+    //     promise.then(() => {
+    //         //here setstate acts like synchronus call
+    //         this.setState({Qty: this.state.Qty+10});
+    //         this.setState({Qty: this.state.Qty+10});
+    //         this.setState({Qty: this.state.Qty+10});
+            
+    //         //as we know we cannot acess the state just below the above statement as it will not reflect the 
+    //         //updated changes as it was asynchronus
+    //         //but here we can as this is synchronus call
+    //         console.log('state', this.state);
+    //     });
+    // }
     //below we use the arrow function for the binding purpose 
     // so whereever we find any problem regarding the binding then we can use the arrow function to rsolve any such problem
-    
+
     increaseQuantity = () => {
         // console.log("this.state",this.state);
-        console.log("new Quantity", ++this.state.Qty);
+        // console.log("new Quantity", ++this.state.Qty);
+        //react doesnot know that we are increasing the value so it is not rerendering the value
+        //we need to tell react that we are increasing the value and this is needed to be rerendered 
+        //so we need to use the function called setstate which comes fro the componennt part of the react 
+        //setstate form 1 (by just giving an object)
+        //using setstate we can trigger or rerender our updated value
+        // this.setState({
+        //     Qty: this.state.Qty+1
+        // });
+        //setstate form 2
+        //if there is the requirement of previous state then we use form2 otherwise we use form 1
+        //setstate calls are asynchronus this means that we dont know when is this call ending 
+        //so we add callback to the both the forms of setstate and this call back will be executed after completion of the setstate
+
+
+        this.setState((prevState) => {
+            
+          return{
+              Qty: prevState.Qty +1
+              
+        }
+    }, () =>{
+        //this is the callback function and will be executed only after completion of above function
+        //
+        console.log('this.state',this.state);
+    });
     }
-    decreaseQuantity(){
-        console.log("decrease");
+
+
+
+
+
+    //using arrow function to bind this 
+    decreaseQuantity = () => {
+        const { Qty } =this.state;
+        if(Qty === 1){
+            return;
+        }
+        // console.log("decrease");
+        //using the setstate form2 
+        this.setState((prevState) => {
+            return{
+                Qty: prevState.Qty -1
+            }    
+        });
     }
     deleteProduct(){
         console.log("Delete");
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 
 
 ReactDOM.render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 
