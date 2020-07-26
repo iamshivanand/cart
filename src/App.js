5f1a0932d182354e35e28fb5807b9c885ac4d0a8
Shiv <iamshivanand@users.noreply.github.com>
2020-07-26 20:16:52 +0530

    database is also added

--- src/App.js
+++ src/App.js
@@ -2,81 +2,162 @@ import React from 'react';
 import './App.css';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import * as firebase from 'firebase';
 
 class App extends React.Component {
   constructor () {
     //we need to also call superclass as we extends from super class react.components
     super();
     this.state = {
-    products: [
-        {
-          price: 999,
-          tittle: 'Phone',
-          Qty:5,
-          img:'https://i.pinimg.com/564x/10/bc/18/10bc18bd1976274b1ac337ea2fd6137b.jpg',
-          id:1
-        },
-        {
-          price: 99,
-          tittle: 'Watch',
-          Qty:1,
-          img:'https://i.pinimg.com/564x/3c/61/84/3c61840c88d19ebdd6c272b8fa72553a.jpg',
-          id:2
-        },
-        {
-          price: 9999,
-          tittle: 'Laptop',
-          Qty:3,
-          img:'https://i.pinimg.com/564x/ca/4c/2a/ca4c2a36e8e2d429cf0e36df17fb38b8.jpg',
-          id:3
-        },
-        {
-          price: 9999,
-          tittle: 'ipad',
-          Qty:2,
-          img:'https://i.pinimg.com/564x/f7/67/4a/f7674a33efd665d08d4a1004094267fb.jpg',
-          id:4
-        }
-    ]
+    products:[],
+    loading:true
 }
+this.db= firebase.firestore();
 }
+
+componentDidMount(){
+  // firebase
+  //   .firestore()
+  //   .collection('products')
+  //   .get()
+  //   .then((snapshot) => {
+  //     console.log('Query snapshot',snapshot);
+
+  //     snapshot.docs.map((doc) => {
+  //       console.log(doc.data());
+  //     })
+
+  //     const products= snapshot.docs.map((doc) => {
+  //      const data= doc.data();
+
+  //      data['id']= doc.id;
+
+  //       return data;
+  //     })
+  //     this.setState({
+  //       products,
+  //       loading:false
+  //     })
+  //   })
+//on snapshot function given below is called whenever 
+// change happen in the database
+ this.db
+  .collection('products')
+  //this is how we can query data from firebase
+  // .where('price','==',999)
+  // .where('tittle','==','bag')
+  //if we want to sort our products according to price
+  .orderBy('price','desc')
+  .onSnapshot((snapshot) => {
+    console.log('Query snapshot',snapshot);
+
+    snapshot.docs.map((doc) => {
+      console.log(doc.data());
+    })
+
+    const products= snapshot.docs.map((doc) => {
+     const data= doc.data();
+
+     data['id']= doc.id;
+
+      return data;
+    })
+    this.setState({
+      products,
+      loading:false
+    })
+  })
+}
+
+
 handleIncreaseQuantity= (product)=>{
-console.log('Hey please increase the quantity ');
-console.log(product);
+// console.log('Hey please increase the quantity ');
+// console.log(product);
 const {products}=this.state;
 const index= products.indexOf(product);
 
-products[index].Qty+=1;
+// products[index].Qty+=1;
 
-this.setState({
-    // products: products
-    //as name of both the above variables are same so we
-    // use shorthand given below
-    products
-});
+// this.setState({
+//     // products: products
+//     //as name of both the above variables are same so we
+//     // use shorthand given below
+//     products
+// });
+
+const docRef= this.db.collection('products').doc(products[index].id);
+//using above we will get the refrence for that product with that particular id
+// console.log( 'DocRef increase is: ', docRef);
+
+docRef
+  .update({
+    Qty: products[index].Qty+1
+  })
+  .then(() => {
+    console.log('Updated Succesfully');
+  })
+  .catch((error) => {
+    console.log('Error',error);
+  })
 }
+
+
+
+
 handleDecreaseQuantity= (product) =>{
 const {products}= this.state;
 const index= products.indexOf(product);
 
-if(products[index].Qty===1){
-    return;
-}
-products[index].Qty-=1;
+// if(products[index].Qty===1){
+//     return;
+// }
+// products[index].Qty-=1;
 
-this.setState({
-    products
-});
+// this.setState({
+//     products
+// });
+const docRef= this.db.collection('products').doc(products[index].id);
+
+if(products[index].Qty>1){
+docRef
+  .update({
+    Qty: products[index].Qty-1
+  })
+  .then(() => {
+    console.log('updated Succesfully');
+  })
+  .catch((error) => {
+    console.log('Error', error);
+  })
 }
+else{
+  return;
+}
+}
+
+
+
+
 handleDeleteProduct= (id) =>{
 const {products}= this.state;
 
-const items= products.filter((item) => item.id !== id); //this will return the arrray of the products whose id doesnot match 
+// const items= products.filter((item) => item.id !== id); //this will return the arrray of the products whose id doesnot match 
 
-this.setState({
-    products:items
-});
+// this.setState({
+//     products:items
+// });
+const docRef= this.db.collection('products').doc(id);
+docRef
+  .delete()
+  .then(() => {
+    console.log('Deleted Succesfully');
+  })
+  .catch((error) => {
+    console.log('Error', error);
+  })
 }
+
+
 getCartCount = () => {
   const{ products }= this.state;
 
@@ -85,9 +166,12 @@ getCartCount = () => {
     count+= product.Qty;
     
   });
-  console.log('count is ',count);
+  // console.log('count is ',count);
   return count;
 }
+
+
+
 getcartTotal= () => {
   const {products}= this.state;
    let totalPrice=0;
@@ -98,20 +182,44 @@ getcartTotal= () => {
    return totalPrice;
 }
 
+
+addProduct= () =>{
+  this.db
+    .collection('products')
+    .add({
+      img:'',
+      price:900,
+      Qty:3,
+      tittle:'washing machine'
+    })//above .add function will return promise and then we call then
+    //docRef in the below function point to our document
+    .then((docRef) => {
+      console.log('Product has been added',docRef);
+    })
+    .catch((error) => {
+      console.log('error :', error);
+    })
+}
+
+
+
   render(){
     
-    const { products } = this.state;
+    const { products ,loading } = this.state;
     return (
       <div className="App">
         <Navbar 
           count={this.getCartCount()}
         />
+        {/* <button onClick={this.addProduct} style= {{padding:20, fontSize:20}}>Add a product</button> */}
         <Cart 
           products= {products}
           increaseQuantity= {this.handleIncreaseQuantity}
           decreaseQuantity= {this.handleDecreaseQuantity}
           deleteProduct= {this.handleDeleteProduct}
         />
+        {/* here we are using conditional rendering  */}
+        {loading && <h1>Loading the products....</h1>}
         <div style= {{fontSize:20,fontFamily:'cursive',padding:10}}>TOTAL: {this.getcartTotal()}
 
         </div>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+import 'firebase/firestore'
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBFXE7MzcifUO9AUrYPdwZZjRwknoS4M7k",
+  authDomain: "cart-59f54.firebaseapp.com",
+  databaseURL: "https://cart-59f54.firebaseio.com",
+  projectId: "cart-59f54",
+  storageBucket: "cart-59f54.appspot.com",
+  messagingSenderId: "955582138584",
+  appId: "1:955582138584:web:d8686170bd1fe22065efad"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   
   <React.StrictMode>
